203577a9000996caf08a3b4f07a7da63cf80711b
Xzenia <probablynotbutmostdefinitely@gmail.com>
2017-12-03 07:57:35 +0800

    Major improvements to the admin GUI

---
+++ Admin/AdminSplashScreen.designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Admin
+{
+    partial class AdminSplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminSplashScreen));
+            this.loadBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tm = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // loadBar
+            // 
+            this.loadBar.BackColor = System.Drawing.Color.DarkCyan;
+            this.loadBar.ForeColor = System.Drawing.Color.DarkOrange;
+            this.loadBar.Location = new System.Drawing.Point(257, 162);
+            this.loadBar.Name = "loadBar";
+            this.loadBar.Size = new System.Drawing.Size(229, 23);
+            this.loadBar.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(258, 214);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "(c) Herbosa Bancud Javier 2017";
+            // 
+            // tm
+            // 
+            this.tm.Interval = 32;
+            this.tm.Tick += new System.EventHandler(this.tm_Tick);
+            // 
+            // AdminSplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(779, 244);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loadBar);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AdminSplashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar loadBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer tm;
+    }
+}
